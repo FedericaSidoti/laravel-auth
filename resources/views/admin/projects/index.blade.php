575ef6cbf738d035b6407e446bd3753f2aa7b871
FedericaSidoti <federicasidoti21@gmail.com>
2023-12-27 09:50:23 +0100

    FIX- table-styled index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -5,6 +5,7 @@
     <h2 class="fs-4 text-secondary my-4">
         Tutti i Progetti
     </h2>
+
     <div class="row justify-content-center">
         <div class="col">
             <div class="card">
@@ -24,45 +25,70 @@
     </div>
 
     <div class="row p-5">
-    @forelse ($projects as $project)
-        <div class="modal fade" id="modal-{{$project->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Attenzione</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <h3>Vuoi davvero eliminare {{$project->title}}?</h3>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Annulla</button>
-                        <form action="{{route('admin.projects.destroy', $project->id)}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Elimina</button>
-                        </form>
+        <table class="table table-striped">
+            <thead class="table-primary">
+                <tr class="text-center">
+                    <th scope="col">#</th>
+                    <th scope="col">IMG</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Descrizione</th>
+                    <th scope="col">Azioni</th>
+                </tr>
+            </thead>
+            <tbody>
+            {{-- Ciclo forelse: projects --}}
+            @forelse ($projects as $project)
+                {{-- Modale --}}
+                <div class="modal fade" id="modal-{{$project->id}}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Attenzione</h5>
+                                    button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <h3>Vuoi davvero eliminare {{$project->title}}?</h3>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Annulla</button>
+                                <form action="{{route('admin.projects.destroy', $project->id)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">Elimina</button>
+                                </form>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        <div class="col-3">
-            <div class="card border-primary">
-                <img src="{{$project->thumb}}">
-                <h2>{{$project->title}}</h2>
-                <p>{{$project->description}}</p>
-                <div class="d-flex justify-content-around">
-                    <button class="btn btn-primary"><a href="{{route('admin.projects.show', $project->id)}}">Dettagli</a></button>
-                    <button class="btn btn-primary"><a href="{{route('admin.projects.edit', $project->id)}}">Modifica</a></button>
-                    <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{$project->id}}">Elimina</button>
-                </div>
-                
-            </div>
-        </div>
-    @empty
-        <h2>Non ci sono progetti da mostrare!</h2>
-    @endforelse
-    </div>
+                {{-- Fine modale --}}
+                <tr>
+                    <th scope="row">
+                        <p class="h-100 d-flex align-items-center py-4">{{$project->id}}</p>
+                    </th>
+                    <td>
+                        <img class="img-table" src="{{$project->thumb}}">
+                    </td>
+                    <td>
+                        <h2 class="py-4">{{$project->title}}</h2>
+                    </td>
+                    <td>
+                        <p class="py-4">{{$project->description}}</p>
+                    </td>
+                    <td>
+                        <div class="d-flex justify-content-around py-4">
+                            <button class="btn btn-primary"><a href="{{route('admin.projects.show', $project->id)}}">Dettagli</a></button>
+                            <button class="btn btn-primary"><a href="{{route('admin.projects.edit', $project->id)}}">Modifica</a></button>
+                            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{$project->id}}">Elimina</button>
+                        </div>
+                    </td>
+                    
+                </tr>
+                @empty
+                    <tr><h2>Non ci sono progetti da mostrare!</h2></tr>
+                @endforelse
+            </tbody>
+        </table>
+    
 
     
 </div>
